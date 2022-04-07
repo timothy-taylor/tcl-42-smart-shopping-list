df382e8e9bd73569fcb940820bd80caa63278acf
Shirene Boyd <shirenekadkhodai@gmail.com>
2022-04-07 14:12:36 -0500

    code correctly listens to data from Firestore

--- src/components/ShowData.js
+++ src/components/ShowData.js
@@ -3,27 +3,30 @@ import { db } from '../lib/firebase';
 import { collection, onSnapshot } from 'firebase/firestore';
 
 const ShowData = () => {
-    const [docs, setDocs] = useState([]);
+  const [docs, setDocs] = useState([]);
 
-    useEffect(() => {
-        const unsubscribe = onSnapshot(collection(db, 'users'), (snapshot) => {
-            const snapshotDocs = [];
-            snapshot.forEach(doc => snapshotDocs.push(doc));
-            setDocs(snapshotDocs);
-        })
-        return () => unsubscribe();
-    }, []);
-
-    const items = docs.map(doc => {
-        console.log(doc._document.data.value.mapValue.fields.item.stringValue);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, 'users'), (snapshot) => {
+      const snapshotDocs = [];
+      snapshot.forEach((doc) => snapshotDocs.push(doc));
+      setDocs(snapshotDocs);
     });
+    return () => unsubscribe();
+  }, []);
 
-    console.log("this is docs--", docs);
-    return (
-            <div>
-                {`${[...docs]}`}
-            </div>
-        )
+  //   console.log('this is docs--', docs);
+
+  return (
+    <div>
+      {docs.map((doc) => {
+        return (
+          <ul>
+            <li>{doc.id}</li>
+          </ul>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ShowData;
