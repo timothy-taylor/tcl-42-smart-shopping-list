5add8815931faf86a2e02a897cf51d47fda0f224
Shirene Boyd <shirenekadkhodai@gmail.com>
2022-04-11 20:18:32 -0500

    Add:form input updating to database

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { db } from '../../lib/firebase';
 import { collection, addDoc } from 'firebase/firestore';
 import Navigation from '../Navigation/Navigation';
 
 const AddItem = () => {
+  const [itemName, setItemName] = useState('');
+  const [purchaseFreq, setPurchaseFreq] = useState('');
+
   async function handleClick(e) {
-    e.preventDefault();
+    setItemName(e.target.value);
+
 
     try {
       const colRef = collection(db, 'users');
       const docRef = await addDoc(colRef, {
-        item: 'bread',
+        item: {itemName},
       });
       console.log('Document written with ID: ', docRef.id);
     } catch (e) {
@@ -18,17 +22,53 @@ const AddItem = () => {
     }
   }
 
+  function handleItemChange(event) {
+  }
+
+
+
   return (
     <>
       <Navigation />
       <h1>Smart Shopping List</h1>
+      <div>
+        <form>
+          <label>
+            <input
+              type="text"
+              required
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
+            ></input>
+            Name
+          </label>
+        </form>
+        <form>
+          <div className="radio">
+            <label>
+              <input type="radio" value="soon" />
+              Soon
+            </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input type="radio" value="kinda" />
+              Kinda soon
+            </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input type="radio" value="not" />
+              Not soon
+            </label>
+          </div>
+        </form>
+      </div>
       <div className="content">
         <form id="add-shopping-list-form"></form>
         <ul id="shopping-list"></ul>
       </div>
-      <button onClick={(e) => handleClick(e)}>
-        Add Data
-      </button>
+      <button onClick={(e) => handleClick(e)}>Add Data</button>
     </>
   );
 };
