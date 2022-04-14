7382d46ffff1fac3d0e219f16e668aaccf16a311
Alex Bahl <50714034+alexbahl@users.noreply.github.com>
2022-04-13 19:04:36 -0600

    Updates List.js to pull data from firestore live correctly

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../../lib/firebase';
-import { collection, onSnapshot } from 'firebase/firestore';
+import { collection, onSnapshot} from 'firebase/firestore';
 import Navigation from '../Navigation/Navigation';
 
 const List = () => {
   const [docs, setDocs] = useState([]);
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(collection(db, 'users'), (snapshot) => {
-      const snapshotDocs = [];
-      snapshot.forEach((doc) => snapshotDocs.push(doc));
-      setDocs(snapshotDocs);
-    });
-    return () => unsubscribe();
+
+    const unsub = onSnapshot(collection(db, "users"), (snapshot) => {
+      setDocs(snapshot.docs.map(doc => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        }
+      }));
+  });
+    return unsub;
   }, []);
 
   return (
@@ -20,7 +24,7 @@ const List = () => {
       <Navigation />
       <ul>
         {docs.map((doc) => {
-          return <li key={doc.id}>{doc.id}</li>;
+          return <li key={doc.id}>{doc.item}</li>;
         })}
       </ul>
     </>
