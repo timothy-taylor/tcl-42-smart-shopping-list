bce15fccaaeeab47c26122b60c6b8c6920b90fde
Shirene Boyd <shirenekadkhodai@gmail.com>
2022-04-25 19:30:11 -0500

    ADD: conditional for empty list

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../../lib/firebase';
-import { collection, onSnapshot } from 'firebase/firestore';
+import { collection, doc, onSnapshot } from 'firebase/firestore';
 import Navigation from '../Navigation/Navigation';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 const List = () => {
   const { token } = useParams();
   const [docs, setDocs] = useState([]);
 
   useEffect(() => {
-    const unsub = onSnapshot(collection(db, token || "example"), (snapshot) => {
+    const unsub = onSnapshot(collection(db, token || 'example'), (snapshot) => {
       setDocs(
         snapshot.docs.map((doc) => {
           return {
@@ -24,12 +24,24 @@ const List = () => {
 
   return (
     <>
+      <h1>Smart Shopping List</h1>
       <Navigation token={token} />
-      <ul>
-        {docs.map((doc) => {
-          return <li key={doc.id}>{doc.item}</li>;
-        })}
-      </ul>
+      {docs.length === 0 ? (
+        <div>
+          <p>Your shopping list is currently empty</p>
+          <Link to={`/addItem/${token}`}>
+            <button>Add Item</button>
+          </Link>
+        </div>
+      ) : (
+        <>
+          <ul>
+            {docs.map((doc) => {
+              return <li key={doc.id}>{doc.item}</li>;
+            })}
+          </ul>{' '}
+        </>
+      )}
     </>
   );
 };
